83b5ef2ff67099563300fad34eba3f354185062c
David Calculli <david@calculli.com>
2019-01-04 19:01:12 +1100

    Changes to lookup algorithm to be more accurate

--- owm-weather.py
+++ owm-weather.py
@@ -6,6 +6,7 @@ from pyowm.exceptions.api_call_error import APICallError
 
 LOC_NOT_FOUND_MSG = "Could not find your location. Try refining with <city>,<two-letter-country-code> such as Melbourne,AU or Melbourne,US"
 API_OFFLINE_MSG = "The OpenWeatherMap API is not currently online. Try again later."
+OBSERVATION_LOOKUP_LIMIT = 3
 
 try:
     import sopel
@@ -19,37 +20,24 @@ except ImportError:
 
 
 @sopel.module.commands('weather', 'wea')
-@sopel.module.example('.weather London')
+@sopel.module.example('.weather London', 'Gets the weather for London and returns the first result. Use a country suffix to get more accurate information, such as London,GB.',
+        'Can also use a latitude,longitude decimal notation search, along with a specific OWM ID search')
 @sopel.module.rate(server=1)
 def weather(bot, trigger):
     """ .weather location - show the weather at a given location """
-    location = trigger.group(2)
-    if not location:
-        location = bot.db.get_nick_value(trigger.nick, 'place_id')
-        if not location:
+    location_lookup = trigger.group(2)
+    if not location_lookup:
+        location_lookup = bot.db.get_nick_value(trigger.nick, 'place_id')
+        if not location_lookup:
             bot.reply("I don't know where you live. "
                 "Give me a location, like {pfx}{command} London, "
                 "or tell me where you live by saying {pfx}setlocation "
                 "London, for example.".format(command=trigger.group(1),
                 pfx=bot.config.core.help_prefix)) 
-
-
-    try:
-        api = bot.memory['owm']['api']
-        observation = lookup_observation(api, location)
-        location = "{},{}".format(observation.get_location().get_name(), observation.get_location().get_country())
-        weather = observation.get_weather()
-        uv = get_uv_index(api, observation.get_location())
-        message = format_weather_message(location, weather, uv)
-    except NotFoundError:
-        global LOC_NOT_FOUND_MSG
-        message = LOC_NOT_FOUND_MSG
-    except UnauthorizedError as ue:
-        message = str(ue)
-    except APICallError:
-        global API_OFFLINE_MSG
-        message = API_OFFLINE_MSG
-
+            return
+    
+    api = bot.memory['owm']['api']
+    message = get_weather(api, location_lookup)
     say_info(bot, message)
 
 @sopel.module.commands('setlocation', 'setcityid')
@@ -62,12 +50,26 @@ def setlocation(bot, trigger):
 
     api = bot.memory['owm']['api']
     location_lookup = trigger.group(2)
-    location_list = lookup_location(api, location_lookup)
+    location_group_list = lookup_location(api, location_lookup)
+    location_list = []
+    if len(location_group_list) == 0: 
+        # Attempt to load observations based on the nearest place to this lookup
+        # and pull the locations from that set
+        observations = lookup_observations_at_place(api, location_lookup)
+        for obs in observations:
+            location_list.append(obs.get_location())
+    else:
+        # Load locations based on the group list supplied with a direct lookup
+        for lg in location_group_list:
+            location_name = lg['name']
+            location_group = lg['locations'] 
+            if len(location_group) > 0: 
+                location_list.append({'name':location_name,'location':location_group[0]})
+
     if len(location_list) > 1:
         location_refine_message = "Please refine your location by adding a country code. Valid options are: {}".format(str(list(map(lambda x: x['name'], location_list))).strip('[]'))
         bot.reply(location_refine_message)
     elif len(location_list) == 0:
-        global LOC_NOT_FOUND_MSG
         bot.reply(LOC_NOT_FOUND_MSG)
     else:
         location = location_list.pop()
@@ -101,6 +103,65 @@ def shutdown(bot):
     del bot.memory['owm']
 
 # --- End Sopel Code Section ---
+
+def get_weather(api, location_lookup):
+    """ Gets the weather at a location or any error messages """
+    try:
+        approx_observation = False
+        if is_place_id(location_lookup):
+            observation = api.weather_at_id(int(location_lookup))
+        elif is_place_coords(location_lookup):
+            coords = get_place_coords(location_lookup)
+            observation = api.weather_at_coords(coords[0], coords[1])
+        else:
+            location_group_list = lookup_location(api, location_lookup)
+            locations = None
+            if len(location_group_list) == 0:
+                approx_observation = True
+
+            if approx_observation:
+                # Do an approximate search for location
+                observations = lookup_observations_at_place(api, location_lookup)
+            else:
+                # Get the first result from our location group list
+                location_group_set = location_group_list[0]
+                location_group = location_group_set['locations']
+                observations = []
+                uvs = []
+                location = location_group[0]
+                observations = lookup_observations_at_place(api, "{},{}".format(location.get_name(), location.get_country()), 'accurate')
+                uvs.append(get_uv_index(api, location))
+
+            if len(observations) == 0: 
+                raise NotFoundError(cause='No observations found')
+            
+            observation = observations[0]
+
+        # Format our observation into the appropriate message
+        location_text = "{},{}".format(observation.get_location().get_name(), observation.get_location().get_country())
+        if approx_observation:
+            # Highlight that an approximate location was found
+            location_text = "{}(*)".format(location_text)
+
+        weather = observation.get_weather()
+        uv = get_uv_index(api, observation.get_location())
+        message = format_weather_message(location_text, weather, uv)
+    except NotFoundError:
+        global LOC_NOT_FOUND_MSG
+        message = LOC_NOT_FOUND_MSG
+    except UnauthorizedError as ue:
+        message = str(ue)
+    except APICallError:
+        global API_OFFLINE_MSG
+        message = API_OFFLINE_MSG
+    
+    return message
+
+
+def get_weather_at_location(api, location):
+    """ Looks up an observation at a specific location id """
+    observation = lookup_observation(api, location.get_id())
+    return observation
 
 def format_weather_message(location, weather, uv):
     cover = get_cover(weather)
@@ -187,33 +248,41 @@ def get_wind(w):
 
     return "{} {}m/s ({})".format(description, speed_m_s, degrees)
 
-def lookup_location(api, location):
+def lookup_location(api, location_lookup):
     """ Looks up a location to see if it's valid """
     registry = api.city_id_registry()
 
     # Split the location on a comma if there is a country
     # e.g., "Wellington,NZ" because that country needs to be
     # supplied as a separate parameter in registry.locations_for
-    loc = location.split(',')
+    loc = location_lookup.split(',')
     loc_lookup = loc[0].strip()
     loc_country = "" 
     if len(loc) > 1:
         loc_country = loc[1].strip()
 
     # Look up the registery for our locations
+    # Available accuracy: 'exact', 'like', 'nocase'
+    lookup_accuracy = 'like'
     if not loc_country:
-        locations = registry.locations_for(loc_lookup, matching='nocase')
+        locations = registry.locations_for(loc_lookup, matching=lookup_accuracy)
     else:
-        locations = registry.locations_for(loc_lookup, country=loc_country, matching='nocase')
+        locations = registry.locations_for(loc_lookup, country=loc_country, matching=lookup_accuracy)
 
-    location_list = [] 
+    # location_list structure is: { "Melbourne,AU": [ multiple, locations ] }
+    location_group_list = [] 
     for l in locations:
-        canonical_name = "{},{}".format(l.get_name(), l.get_country())
-        if not any(d.get('name', None) == canonical_name for d in location_list):
-            location_list.append({'name':canonical_name, 'location':l})
-    
-    location_list.sort(key=lambda x: x['name'])
-    return location_list   
+        # Only append to the location list if the location lookup name is identical to the location name
+        if l.get_name() == loc_lookup:
+            canonical_name = "{},{}".format(l.get_name(), l.get_country())
+            if not any(d.get('name', None) == canonical_name for d in location_group_list):
+                location_group_list.append({'name': canonical_name, 'locations': []})
+
+            location_list = [ll for ll in location_group_list if ll['name'] == canonical_name][0]
+            location_list['locations'].append(l)
+
+    location_group_list.sort(key=lambda x: x['name'])
+    return location_group_list   
 
 def lookup_observation(api, location):
     """ Looks up the observation based on the location value provided """
@@ -226,6 +295,10 @@ def lookup_observation(api, location):
         observation = api.weather_at_place(name=location)
 
     return observation
+
+def lookup_observations_at_place(api, location_lookup, searchtype='like'):
+    observations = api.weather_at_places(location_lookup, searchtype=searchtype, limit=OBSERVATION_LOOKUP_LIMIT)
+    return observations
 
 def lookup_weather(api, location):
     observation = lookup_observation(api, location)
@@ -243,7 +316,7 @@ def get_api(api_key):
     return owm
 
 def is_place_id(location):
-    """ Checks whether or not the city lookup can be cast to an int, representing a WOEID """
+    """ Checks whether or not the city lookup can be cast to an int, representing a OWM Place ID """
     try:
         int(location) 
         return True
@@ -308,21 +381,29 @@ if __name__ == "__main__":
     if location_lookup is None:
         location_lookup = 'Melbourne'
 
+    print("Looking up weather for location '{}'".format(location_lookup))
     api = get_api(api_key)
-    try:
-        print("Getting observation for location {}".format(location_lookup))
-        observation = lookup_observation(api, location_lookup)
-        location = observation.get_location()
-        weather = observation.get_weather()
-        message = format_weather_message(location.get_name(), weather)
-    except NotFoundError:
-        message = LOC_NOT_FOUND_MSG
-
+    message = get_weather(api, location_lookup)
     print(message)
 
 
-    print("Getting locations for location {}".format(location_lookup))
-    location_list = lookup_location(api, location_lookup)
+    print("Getting locations for location '{}'".format(location_lookup))
+    location_group_list = lookup_location(api, location_lookup)
+    location_list = []
+    if len(location_group_list) == 0: 
+        # Attempt to load observations based on the nearest place to this lookup
+        # and pull the locations from that set
+        observations = lookup_observations_at_place(api, location_lookup)
+        for obs in observations:
+            location_list.append(obs.get_location())
+    else:
+        # Load locations based on the group list supplied with a direct lookup
+        for lg in location_group_list:
+            location_name = lg['name']
+            location_group = lg['locations'] 
+            if len(location_group) > 0: 
+                location_list.append({'name':location_name,'location':location_group[0]})
+
     if len(location_list) > 1:
         location_refine_message = "Please refine your location by adding a country code. Valid options are: {}".format(str(list(map(lambda x: x['name'], location_list))).strip('[]'))
         print(location_refine_message)
